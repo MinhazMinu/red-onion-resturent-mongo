7acc2bf7f61b11e35d846fbc171463c275f061af
minhaz <minhaz.taher@gmail.com>
2020-04-05 08:32:29 +0600

    home page fake data remove

--- src/Components/Foods/Foods.js
+++ src/Components/Foods/Foods.js
@@ -1,56 +1,94 @@
-import React from 'react';
-import {span, Link} from 'react-router-dom';
-import './Foods.css'
-import FoodItem from '../FoodItem/FoodItem';
-import AllFoods from '../../Data/foods.json';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React from "react";
+import { span, Link } from "react-router-dom";
+import "./Foods.css";
+import FoodItem from "../FoodItem/FoodItem";
+
+import { useState } from "react";
+import { useEffect } from "react";
 const Foods = (props) => {
-    const [foods, setFoods] = useState([]);
-    const [selectedFoodType, setSelectedFoodType] = useState("Breakfast");
-    useEffect(() => {
-        setFoods(AllFoods);
-    } ,[])
-    const selectedFoods =  foods.filter(food => food.type == selectedFoodType)
-    
-    console.log(selectedFoodType);
-    return (
-        <section className="food-area my-5">
-            <div className="container">
-                <nav>
-                    <ul className="nav justify-content-center">
-                        <li onClick={() => setSelectedFoodType("Breakfast")} className="nav-item">
-                            <span  to="breakfast" className={selectedFoodType === "Breakfast" ?  "active nav-link" : "nav-link"}>Breakfast</span>
-                        </li>
-                        <li onClick={() => setSelectedFoodType("Lunch")} className="nav-item">
-                            <span to="breakfast" className={selectedFoodType === "Lunch" ?  "active nav-link" : "nav-link"}>Lunch</span>
-                        </li>
-                        <li onClick={() => setSelectedFoodType("Dinner")} className="nav-item">
-                            <span to="breakfast" className={selectedFoodType === "Dinner" ?  "active nav-link" : "nav-link"}>Dinner</span>
-                        </li>
-                    </ul>
-                </nav>
+  const [foods, setFoods] = useState([]);
+  const [selectedFoodType, setSelectedFoodType] = useState("Breakfast");
+  useEffect(() => {
+    fetch("http://localhost:4200/foods")
+      .then((res) => res.json())
+      .then((data) => {
+        setFoods(data);
+      });
+    //
+  }, []);
+  const selectedFoods = foods.filter((food) => food.type == selectedFoodType);
 
-                <div className="row my-5">
-                    {
-                        selectedFoods.map(food => <FoodItem key={food.id} food={food}></FoodItem>)
-                    }
-                </div>
-                <div className="text-center">
-                    {
-                        props.cart.length ? 
-                        <Link to="/checkout">
-                            <button  className="btn btn-danger btn-secondary">Check Out Your Food</button>
-                        </Link>
-                        :
-                        <button disabled className="btn btn-secondary">Check Out Your Food</button>
+  console.log(selectedFoodType);
+  return (
+    <section className="food-area my-5">
+      <div className="container">
+        <nav>
+          <ul className="nav justify-content-center">
+            <li
+              onClick={() => setSelectedFoodType("Breakfast")}
+              className="nav-item"
+            >
+              <span
+                to="breakfast"
+                className={
+                  selectedFoodType === "Breakfast"
+                    ? "active nav-link"
+                    : "nav-link"
+                }
+              >
+                Breakfast
+              </span>
+            </li>
+            <li
+              onClick={() => setSelectedFoodType("Lunch")}
+              className="nav-item"
+            >
+              <span
+                to="breakfast"
+                className={
+                  selectedFoodType === "Lunch" ? "active nav-link" : "nav-link"
+                }
+              >
+                Lunch
+              </span>
+            </li>
+            <li
+              onClick={() => setSelectedFoodType("Dinner")}
+              className="nav-item"
+            >
+              <span
+                to="breakfast"
+                className={
+                  selectedFoodType === "Dinner" ? "active nav-link" : "nav-link"
+                }
+              >
+                Dinner
+              </span>
+            </li>
+          </ul>
+        </nav>
 
-                    }
-
-                </div>
-            </div>
-        </section>
-    );
+        <div className="row my-5">
+          {selectedFoods.map((food) => (
+            <FoodItem key={food.id} food={food}></FoodItem>
+          ))}
+        </div>
+        <div className="text-center">
+          {props.cart.length ? (
+            <Link to="/checkout">
+              <button className="btn btn-danger btn-secondary">
+                Check Out Your Food
+              </button>
+            </Link>
+          ) : (
+            <button disabled className="btn btn-secondary">
+              Check Out Your Food
+            </button>
+          )}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Foods;
