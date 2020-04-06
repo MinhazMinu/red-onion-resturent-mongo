0024fafa9d15c5fa540473ab43c76398c857a548
minhaz <minhaz.taher@gmail.com>
2020-04-06 09:02:43 +0600

    fake data removed from single product

--- src/Components/FoodDetails/FoodDetails.js
+++ src/Components/FoodDetails/FoodDetails.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./FoodDetails.css";
-import allFoods from "../../Data/foods.json";
+// import allFoods from "../../Data/foods.json";
 import { useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
@@ -8,7 +8,19 @@ import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
 const FoodDetails = (props) => {
   const { id } = useParams();
 
-  const currentFood = allFoods.find((food) => food.id == id);
+  const [currentFood, setCurrentFood] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:4200/food/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+
+        setCurrentFood(data);
+      });
+  }, []);
+
+  // let currentFood = allFoods.find((food) => food.id == id);
+
   const [quantity, setQuantity] = useState(1);
   useEffect(() => {
     if (currentFood.quantity) {
@@ -24,39 +36,53 @@ const FoodDetails = (props) => {
     <div className="food-details my-5 container">
       <div className="row">
         <div className="col-md-6 pr-md-4">
-          <h1>{currentFood.name}</h1>
-          <p className="my-5">{currentFood.fullDescription}</p>
-          <div className="d-flex  my-4">
-            <h2 className="price">${currentFood.price.toFixed(2)}</h2>
+          <div>
+            <h1>{currentFood.name}</h1>
+            <p className="my-5">{currentFood.fullDescription}</p>
+            <div className="d-flex  my-4">
+              <h2 className="price">${currentFood.price}</h2>
 
-            <div className="cart-controller ml-3 btn">
-              <button
-                className="btn"
-                onClick={() => setQuantity(quantity <= 1 ? 1 : quantity - 1)}
-              >
-                -
-              </button>{" "}
-              {quantity}{" "}
-              <button className="btn" onClick={() => setQuantity(quantity + 1)}>
-                +
-              </button>
+              <div className="cart-controller ml-3 btn">
+                <button
+                  className="btn"
+                  onClick={() => setQuantity(quantity <= 1 ? 1 : quantity - 1)}
+                >
+                  -
+                </button>{" "}
+                {quantity}{" "}
+                <button
+                  className="btn"
+                  onClick={() => setQuantity(quantity + 1)}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <button
+              className="btn btn-danger btn-rounded mb-2"
+              onClick={() => finalCartHandler(currentFood)}
+            >
+              <FontAwesomeIcon icon={faCartArrowDown} /> Add
+            </button>
+
+            <div className="more-images mt-5 ">
+              {
+                <img
+                  className="mr-4"
+                  height="150px"
+                  src={currentFood.images}
+                  alt=""
+                />
+              }
+              {/* {currentFood.images.map((img) => (
+                  <img className="mr-4" height="150px" src={img} alt="" />
+                ))} */}
             </div>
           </div>
-          <button
-            className="btn btn-danger btn-rounded mb-2"
-            onClick={() => finalCartHandler(currentFood)}
-          >
-            <FontAwesomeIcon icon={faCartArrowDown} /> Add
-          </button>
-
-          <div className="more-images mt-5 ">
-            {currentFood.images.map((img) => (
-              <img className="mr-4" height="150px" src={img} alt="" />
-            ))}
-          </div>
         </div>
+
         <div className="col-md-6">
-          <img className="img-fluid" src={currentFood.images[0]} alt="" />
+          <img className="img-fluid" src={currentFood.images} alt="" />
         </div>
       </div>
     </div>
